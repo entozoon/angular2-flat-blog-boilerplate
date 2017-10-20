fe2e4476a8afe6c09a86d761165481b2cc03fd63
Michael Cook <michael.cook@techdept.co.uk>
2017-10-20 12:47:05 +0100

    Baaasic search functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { HttpModule } from '@angular/http';
 
+import { FormsModule } from '@angular/forms'; // for (ngModel)
+
 import { AppComponent } from './app.component';
 import { ArticleListComponent } from './article/article-list.component';
 import { ArticleComponent } from './article/article.component';
 
 @NgModule({
   declarations: [AppComponent, ArticleListComponent, ArticleComponent],
-  imports: [BrowserModule, HttpModule],
+  imports: [BrowserModule, HttpModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/article/article-list.component.ts
+++ src/app/article/article-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { NgModule } from '@angular/core';
 import { ArticleService } from '../article.service';
 
 @Component({
@@ -9,14 +9,32 @@ import { ArticleService } from '../article.service';
   providers: [ArticleService]
 })
 export class ArticleListComponent implements OnInit {
-  public articles;
+  public articlesCollection; // Entire collection of articles
+  public articles; // Articles that are currently visible
+  public searchQuery;
 
   constructor(private ArticleService: ArticleService) {
+    // On first load, ask the ArticleService to get all the articles and save them to a variable
     this.ArticleService.getTheArticles().then(articles => {
+      this.articlesCollection = articles;
       this.articles = articles;
-      // We have the articles now!
+      // We have the articles now! Site complete! Not.
       console.log(this.articles);
     });
+  }
+
+  search() {
+    // No need to pass the query as a parameter, it's already two-way bound to this.searchQuery
+    console.log('Searching: ' + this.searchQuery);
+
+    // Filter articles
+
+    // Simply search for the query within the article titles (ignoring case)
+    this.articles = this.articlesCollection.filter(article => {
+      return article.title.toLowerCase().includes(this.searchQuery.toLowerCase());
+    });
+
+    // Add to the results any content matches for the query.. but not if already in the results, erm..
   }
 
   ngOnInit() {}
